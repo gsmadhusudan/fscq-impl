36458d537b08d5da9b65032d5a41603b167609aa
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-26 11:25:10 -0400

    BmapAllocAll.v: eventual semantics for complete block allocator
    semantics done, proof remains, depends on BmapAllocOne.v proof too

---
+++ src/BmapAllocAll.v
@@ -0,0 +1,53 @@
+Require Import CpdtTactics.
+Require Import FsTactics.
+Require Import Util.
+Require Import Smallstep.
+Require Import Closures.
+Require Import Tactics.
+Require Import FsLayout.
+Require Import Arith.
+Require Import FunctionalExtensionality.
+Require Import BmapLayout.
+Require Import BmapAllocOne.
+Open Scope fscq.
+
+
+Module BmapAllocAll <: SmallStepLang.
+
+Inductive prog {R:Type} : Type :=
+  | Alloc (rx:option BlocksPartDisk.addr->prog)
+  | Free (a:BlocksPartDisk.addr) (rx:unit->prog)
+  | Return (v:R).
+Definition Prog := @prog.
+Definition ReturnOp := @Return.
+Definition State := BlocksPartDisk.addr -> bool.
+
+Definition freebit (bm:State) (off:BlocksPartDisk.addr) :=
+  bm off = true.
+
+Inductive step {R:Type} : @progstate R Prog State ->
+                          @progstate R Prog State -> Prop :=
+  | StepAllocOK: forall d off rx,
+    highest (freebit d) off ->
+    step (PS (Alloc rx) d)
+         (PS (rx (Some off)) (setidx BlocksPartDisk.eq_addr_dec d off false))
+  | StepAllocOut: forall d rx,
+    (~exists off, freebit d off) ->
+    step (PS (Alloc rx) d)
+         (PS (rx None) d)
+  | StepFree: forall d off rx,
+    step (PS (Free off rx) d)
+         (PS (rx tt) (setidx BlocksPartDisk.eq_addr_dec d off true)).
+Definition Step := @step.
+
+End BmapAllocAll.
+
+
+Module BmapAllocAllToAllocOne <: Refines BmapAllocAll BmapAllocOne.
+
+Obligation Tactic := Tactics.program_simpl; omega'.
+
+(* XXX *)
+
+
+End BmapAllocAllToAllocOne.
