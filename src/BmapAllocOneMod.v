f2cd9910625cb2cd707c899057ba1b28e2cb4361
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-26 23:34:44 -0400

    BmapAllocOneMod.v: finish proof
    prove per-blockmap allocator

--- src/BmapAllocOneMod.v
+++ src/BmapAllocOneMod.v
@@ -13,6 +13,32 @@ Open Scope fscq.
 
 Definition highest {SigP:nat->Prop} (P:sig SigP->Prop) (a:sig SigP) :=
   P a /\ ~exists a', proj1_sig a' > proj1_sig a /\ P a'.
+Definition highest_below {SigP:nat->Prop} (P:sig SigP->Prop) (bound:nat) (a:sig SigP) :=
+  @highest SigP (fun x => P x /\ proj1_sig x < bound) a.
+
+Lemma highest_below_next:
+  forall SigBound P bound a (Hbound: bound<SigBound),
+  @highest_below (fun x => x < SigBound) P (S bound) a ->
+  ~P (exist _ bound Hbound) ->
+  @highest_below (fun x => x < SigBound) P bound a.
+Proof.
+  intros. destruct H. Tactics.destruct_pairs.
+  destruct (eq_nat_dec bound (proj1_sig a)); subst.
+  - destruct H0. rewrite exist_proj_sig. auto.
+  - split; [split|]; [ auto | omega' | ].
+    unfold not; intros. apply H1.
+    destruct H3. exists x. crush.
+Qed.
+
+Lemma highest_below_bound:
+  forall bound P a,
+  @highest (fun x => x < bound) P a ->
+  @highest_below (fun x => x < bound) P bound a.
+Proof.
+  intros. destruct H.
+  split; [split|]. auto. omega'.
+  unfold not; intros. destruct H0. destruct H1. exists x. crush.
+Qed.
 
 Definition eq_blockmapoff_dec (a b:blockmapoff) : {a=b}+{a<>b}.
   refine (if eq_nat_dec (proj1_sig a) (proj1_sig b) then _ else _).
@@ -58,11 +84,20 @@ Module BmapAllocOneToStore <: Refines BmapAllocOne BmapStore.
 
 Obligation Tactic := Tactics.program_simpl; omega'.
 
-Fixpoint bmfree (R:Type) (a:blockmapnum) (off:blockmapoff) rx :
-                BmapStore.Prog R :=
+Definition bmfree (R:Type) (a:blockmapnum) (off:blockmapoff) rx :
+                  BmapStore.Prog R :=
   bm <- BmapStore.Read a;
   BmapStore.Write a (setidx eq_blockmapoff_dec bm off Avail);;
   rx.
+
+Definition bmalloc_helper (R:Type) (bm:blockmap) (off:blockmapoff)
+                          (rxok:blockmapoff->BmapStore.Prog R)
+                          (rxfail:BmapStore.Prog R) :
+                          BmapStore.Prog R :=
+  match bm off with
+  | InUse => rxfail
+  | Avail => rxok off
+  end.
 
 Program Fixpoint bmalloc (R:Type) (a:blockmapnum)
                          (n:nat) (NOK:n<=SizeBlock)
@@ -72,12 +107,11 @@ Program Fixpoint bmalloc (R:Type) (a:blockmapnum)
   | O => rx None
   | S x =>
     bm <- BmapStore.Read a;
-    match bm x with
-    | InUse => bmalloc R a x _ rx
-    | Avail =>
-      BmapStore.Write a (setidx eq_blockmapoff_dec bm x InUse);;
-      rx (Some x)
-    end
+    bmalloc_helper R bm x
+                   (fun off =>
+                    BmapStore.Write a (setidx eq_blockmapoff_dec bm off InUse);;
+                    rx (Some off))
+                   (bmalloc R a x _ rx)
   end.
 
 Program Fixpoint Compile {R:Type} (p:BmapAllocOne.Prog R) : (BmapStore.Prog R) :=
@@ -96,6 +130,52 @@ Inductive statematch : BmapAllocOne.State -> BmapStore.State -> Prop :=
 Definition StateMatch := statematch.
 Hint Constructors statematch.
 
+Lemma bmalloc_fail:
+  forall R n Hn a rx d,
+  (~exists off, BmapAllocOne.freebit (d a) off) ->
+  star (BmapStore.Step R)
+    (PS (bmalloc R a n Hn rx) d)
+    (PS (rx None) d).
+Proof.
+  induction n; intros.
+  - apply star_refl.
+  - eapply star_step; [constructor|].
+    fold bmalloc. unfold bmalloc_helper.
+    match goal with
+    | [ |- context[d a ?n] ] =>
+      case_eq (d a n); intros; subst;
+      [ apply IHn; auto
+      | destruct H; exists n; crush ]
+    end.
+Qed.
+
+Lemma bmalloc_ok:
+  forall R n Hn a rx d off,
+  highest_below (BmapAllocOne.freebit (d a)) n off ->
+  star (BmapStore.Step R)
+    (PS (bmalloc R a n Hn rx) d)
+    (PS (rx (Some off))
+        (setidx eq_blockmapnum_dec d a
+         (setidx eq_blockmapoff_dec (d a) off InUse))).
+Proof.
+  induction n; intros.
+  - destruct H. crush.
+  - eapply star_step; [constructor|].
+    fold bmalloc. unfold bmalloc_helper.
+    match goal with
+    | [ H: highest_below _ _ _ |- _ ] => inversion H
+    end; Tactics.destruct_pairs.
+    destruct (eq_nat_dec n (proj1_sig off)).
+    + generalize H0; subst; intros. repeat rewrite exist_proj_sig; crush.
+      eapply star_step; [constructor|]. apply star_refl.
+    + match goal with
+      | [ |- context[d a ?n] ] =>
+        case_eq (d a n); intros;
+        [ apply IHn; apply highest_below_next with (Hbound:=Hn); crush
+        | destruct H1; exists n; crush ]
+      end.
+Qed.
+
 Theorem FSim:
   forall R,
   forward_simulation (BmapAllocOne.Step R) (BmapStore.Step R).
@@ -112,13 +192,24 @@ Proof.
   end.
 
   - (* Alloc Some *)
-    admit.
+    econstructor; split.
+    + apply bmalloc_ok.
+      apply highest_below_bound. eauto.
+    + crush.
 
   - (* Alloc None *)
-    admit.
+    econstructor; split.
+    + apply bmalloc_fail; auto.
+    + crush.
 
   - (* Free *)
-    admit.
+    econstructor; split.
+    + simpl. unfold bmfree.
+      eapply star_step; [constructor|].
+      eapply star_step; [constructor|].
+      cbv beta.
+      apply star_refl.
+    + constructor; auto.
 Qed.
 
 End BmapAllocOneToStore.
