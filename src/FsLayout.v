971ea3d403276e5c1ad467be0898388a8791b8fc
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-27 16:22:25 -0400

    LogLayout.v: store both log and data on a single disk
    semantics, compile, match, and proof done (largely a clone of FsLayout)

--- src/FsLayout.v
+++ src/FsLayout.v
@@ -26,16 +26,16 @@ Module BlocksPartSize <: DiskSize.
 Definition Size := NBlockMap * SizeBlock.
 End BlocksPartSize.
 
-Module WholeDiskSize <: DiskSize.
+Module FsDiskSize <: DiskSize.
 Definition Size := InodePartSize.Size + BmapPartSize.Size + BlocksPartSize.Size.
-End WholeDiskSize.
+End FsDiskSize.
 
 
 Module InodePartDisk := Disk InodePartSize.
 Module BmapPartDisk := Disk BmapPartSize.
 Module BlocksPartDisk := Disk BlocksPartSize.
 Module FsPartsDisk := FsParts InodePartDisk BmapPartDisk BlocksPartDisk.
-Module WholeDisk := Disk WholeDiskSize.
+Module FsDisk := Disk FsDiskSize.
 
 
 Ltac omega'unfold unfoldt :=
@@ -49,7 +49,7 @@ Ltac omega'unfold unfoldt :=
   omega.
 
 Ltac unfold_fslayout :=
-  unfold WholeDiskSize.Size in *;
+  unfold FsDiskSize.Size in *;
   unfold InodePartSize.Size in *;
   unfold BmapPartSize.Size in *;
   unfold BlocksPartSize.Size in *;
@@ -59,70 +59,69 @@ Ltac unfold_fslayout :=
 Ltac omega' :=
   omega'unfold unfold_fslayout.
 
-Module FsPartsOnDisk <: Refines FsPartsDisk WholeDisk.
+Module FsPartsOnDisk <: Refines FsPartsDisk FsDisk.
 
-Program Definition i2w (n:InodePartDisk.addr) : WholeDisk.addr :=
+Obligation Tactic := Tactics.program_simpl; omega'.
+
+Program Definition i2w (n:InodePartDisk.addr) : FsDisk.addr :=
   n.
-Solve Obligations using intros; omega'.
 
-Program Definition m2w (n:BmapPartDisk.addr) : WholeDisk.addr :=
+Program Definition m2w (n:BmapPartDisk.addr) : FsDisk.addr :=
   n + InodePartSize.Size.
-Solve Obligations using intros; omega'.
 
-Program Definition b2w (n:BlocksPartDisk.addr) : WholeDisk.addr :=
+Program Definition b2w (n:BlocksPartDisk.addr) : FsDisk.addr :=
   n + InodePartSize.Size + BmapPartSize.Size.
-Solve Obligations using intros; omega'.
 
 Ltac omega'' := unfold i2w in *; unfold m2w in *; unfold b2w in *; auto; omega'.
 
 Fixpoint CompileI (R T:Type) (p:InodePartDisk.Prog T)
-                             (rx:T->WholeDisk.Prog R) : WholeDisk.Prog R :=
+                             (rx:T->FsDisk.Prog R) : FsDisk.Prog R :=
   match p with
   | InodePartDisk.Read n rx' =>
-    a <- WholeDisk.Read (i2w n) ;
+    a <- FsDisk.Read (i2w n) ;
     CompileI R T (rx' a) rx
   | InodePartDisk.Write n b rx' =>
-    WholeDisk.Write (i2w n) b ;;
+    FsDisk.Write (i2w n) b ;;
     CompileI R T (rx' tt) rx
   | InodePartDisk.Return r =>
     rx r
   end.
 
 Fixpoint CompileM (R T:Type) (p:BmapPartDisk.Prog T)
-                             (rx:T->WholeDisk.Prog R) : WholeDisk.Prog R :=
+                             (rx:T->FsDisk.Prog R) : FsDisk.Prog R :=
   match p with
   | BmapPartDisk.Read n rx' =>
-    a <- WholeDisk.Read (m2w n) ;
+    a <- FsDisk.Read (m2w n) ;
     CompileM R T (rx' a) rx
   | BmapPartDisk.Write n b rx' =>
-    WholeDisk.Write (m2w n) b ;;
+    FsDisk.Write (m2w n) b ;;
     CompileM R T (rx' tt) rx
   | BmapPartDisk.Return r =>
     rx r
   end.
 
 Fixpoint CompileB (R T:Type) (p:BlocksPartDisk.Prog T)
-                             (rx:T->WholeDisk.Prog R) : WholeDisk.Prog R :=
+                             (rx:T->FsDisk.Prog R) : FsDisk.Prog R :=
   match p with
   | BlocksPartDisk.Read n rx' =>
-    a <- WholeDisk.Read (b2w n) ;
+    a <- FsDisk.Read (b2w n) ;
     CompileB R T (rx' a) rx
   | BlocksPartDisk.Write n b rx' =>
-    WholeDisk.Write (b2w n) b ;;
+    FsDisk.Write (b2w n) b ;;
     CompileB R T (rx' tt) rx
   | BlocksPartDisk.Return r =>
     rx r
   end.
 
-Fixpoint Compile {R:Type} (p:FsPartsDisk.Prog R) : (WholeDisk.Prog R) :=
+Fixpoint Compile {R:Type} (p:FsPartsDisk.Prog R) : (FsDisk.Prog R) :=
   match p with
   | FsPartsDisk.I T p rx => CompileI R T p (fun x => Compile (rx x))
   | FsPartsDisk.M T p rx => CompileM R T p (fun x => Compile (rx x))
   | FsPartsDisk.B T p rx => CompileB R T p (fun x => Compile (rx x))
-  | FsPartsDisk.Return r => WholeDisk.Return r
+  | FsPartsDisk.Return r => FsDisk.Return r
   end.
 
-Inductive statematch : FsPartsDisk.State -> WholeDisk.State -> Prop :=
+Inductive statematch : FsPartsDisk.State -> FsDisk.State -> Prop :=
   | Match: forall i m b d
     (I: forall (a:InodePartDisk.addr), i a = d (i2w a))
     (M: forall (a:BmapPartDisk.addr), m a = d (m2w a))
@@ -141,7 +140,7 @@ Ltac resolve_disks :=
 
 Theorem FSim:
   forall R,
-  forward_simulation (FsPartsDisk.Step R) (WholeDisk.Step R).
+  forward_simulation (FsPartsDisk.Step R) (FsDisk.Step R).
 Proof.
   intros; fsim_begin (@Compile R) statematch.
 
@@ -175,7 +174,7 @@ Proof.
         end
       end; subst;
       [ constructor; eauto
-      | instantiate (1:=(setidx WholeDisk.eq_addr_dec s2 (i2w a) v)); resolve_disks
+      | instantiate (1:=(setidx FsDisk.eq_addr_dec s2 (i2w a) v)); resolve_disks
       | instantiate (2:=m); resolve_disks
       | instantiate (2:=b); resolve_disks
       | exists x; split;
@@ -219,7 +218,7 @@ Proof.
       end; subst;
       [ constructor; eauto
       | instantiate (2:=i);
-        instantiate (1:=(setidx WholeDisk.eq_addr_dec s2 (m2w a) v)); resolve_disks
+        instantiate (1:=(setidx FsDisk.eq_addr_dec s2 (m2w a) v)); resolve_disks
       | resolve_disks
       | instantiate (2:=b); resolve_disks
       | exists x; split;
@@ -263,7 +262,7 @@ Proof.
       end; subst;
       [ constructor; eauto
       | instantiate (2:=i);
-        instantiate (1:=(setidx WholeDisk.eq_addr_dec s2 (b2w a) v)); resolve_disks
+        instantiate (1:=(setidx FsDisk.eq_addr_dec s2 (b2w a) v)); resolve_disks
       | instantiate (2:=m); resolve_disks
       | resolve_disks
       | exists x; split;
---
+++ src/LogLayout.v
@@ -0,0 +1,191 @@
+Require Import CpdtTactics.
+Require Import FsTactics.
+Require Import DiskMod.
+Require Import Util.
+Require Import Smallstep.
+Require Import Closures.
+Require Import Tactics.
+Require Import LogPartsMod.
+Require Import FsLayout.
+Open Scope fscq.
+
+
+Definition LogHeader := 2.
+Definition LogEntries := 20.
+Definition SizeLogEntry := 2.
+
+
+Module LogPartSize <: DiskSize.
+Definition Size := LogHeader + LogEntries * SizeLogEntry.
+End LogPartSize.
+
+Module WholeDiskSize <: DiskSize.
+Definition Size := LogPartSize.Size + FsDiskSize.Size.
+End WholeDiskSize.
+
+
+Module LogPartDisk := Disk LogPartSize.
+Module LogPartsDisk := LogParts LogPartDisk FsDisk.
+Module WholeDisk := Disk WholeDiskSize.
+
+
+Ltac unfold_loglayout :=
+  unfold WholeDiskSize.Size in *;
+  unfold LogPartSize.Size in *;
+  unfold LogHeader in *;
+  unfold LogEntries in *;
+  unfold SizeLogEntry in *;
+  unfold_fslayout.
+
+Ltac omega'log :=
+  omega'unfold unfold_loglayout.
+
+Module LogPartsOnDisk <: Refines LogPartsDisk WholeDisk.
+
+Obligation Tactic := Tactics.program_simpl; omega'log.
+
+Program Definition l2w (n:LogPartDisk.addr) : WholeDisk.addr :=
+  n.
+
+Program Definition d2w (n:FsDisk.addr) : WholeDisk.addr :=
+  n + LogPartSize.Size.
+
+Ltac omega''log := unfold l2w in *; unfold d2w in *; auto; omega'log.
+
+Fixpoint CompileL (R T:Type) (p:LogPartDisk.Prog T)
+                             (rx:T->WholeDisk.Prog R) : WholeDisk.Prog R :=
+  match p with
+  | LogPartDisk.Read n rx' =>
+    a <- WholeDisk.Read (l2w n) ;
+    CompileL R T (rx' a) rx
+  | LogPartDisk.Write n b rx' =>
+    WholeDisk.Write (l2w n) b ;;
+    CompileL R T (rx' tt) rx
+  | LogPartDisk.Return r =>
+    rx r
+  end.
+
+Fixpoint CompileD (R T:Type) (p:FsDisk.Prog T)
+                             (rx:T->WholeDisk.Prog R) : WholeDisk.Prog R :=
+  match p with
+  | FsDisk.Read n rx' =>
+    a <- WholeDisk.Read (d2w n) ;
+    CompileD R T (rx' a) rx
+  | FsDisk.Write n b rx' =>
+    WholeDisk.Write (d2w n) b ;;
+    CompileD R T (rx' tt) rx
+  | FsDisk.Return r =>
+    rx r
+  end.
+
+Fixpoint Compile {R:Type} (p:LogPartsDisk.Prog R) : (WholeDisk.Prog R) :=
+  match p with
+  | LogPartsDisk.L T p rx => CompileL R T p (fun x => Compile (rx x))
+  | LogPartsDisk.D T p rx => CompileD R T p (fun x => Compile (rx x))
+  | LogPartsDisk.Return r => WholeDisk.Return r
+  end.
+
+Inductive statematch : LogPartsDisk.State -> WholeDisk.State -> Prop :=
+  | Match: forall ls ds d
+    (L: forall (a:LogPartDisk.addr), ls a = d (l2w a))
+    (D: forall (a:FsDisk.addr), ds a = d (d2w a)),
+    statematch (LogPartsDisk.PartState ls ds) d.
+Definition StateMatch := statematch.
+Hint Constructors statematch.
+
+Ltac resolve_disks :=
+  intros;
+  match goal with
+  | [ |- setidx ?eq _ ?a _ ?b = _ ] => destruct (eq a b)
+  | [ |- _ = setidx ?eq _ ?a _ ?b ] => idtac
+  end;
+  repeat resolve_setidx omega''log; auto.
+
+Theorem FSim:
+  forall R,
+  forward_simulation (LogPartsDisk.Step R) (WholeDisk.Step R).
+Proof.
+  intros; fsim_begin (@Compile R) statematch.
+
+  - (* L *)
+    generalize dependent s2. generalize dependent S.
+    generalize dependent ls. generalize dependent ds.
+    unfold Compile; fold (@Compile R).
+    induction p.
+
+    + intros; simpl;
+      edestruct H; inversion S; clear S;
+      match goal with
+      | [ H: star (?STEP R) _ _ |- _ ] =>
+        inversion H; match goal with
+        | [ Hstep: STEP _ _ _ |- _ ] => inversion Hstep
+        end
+      end; subst;
+      [ constructor; eauto
+      | instantiate (1:=s2); eauto
+      | instantiate (2:=ds); eauto
+      | exists x; split;
+        [ eapply star_step; [constructor|crush] | crush ] ].
+    + intros; simpl;
+      edestruct H; inversion S; clear S;
+      match goal with
+      | [ H: star (?STEP R) _ _ |- _ ] =>
+        inversion H; match goal with
+        | [ Hstep: STEP _ _ _ |- _ ] => inversion Hstep
+        end
+      end; subst;
+      [ constructor; eauto
+      | instantiate (1:=(setidx WholeDisk.eq_addr_dec s2 (l2w a) v)); resolve_disks
+      | instantiate (2:=ds); resolve_disks
+      | exists x; split;
+        [ eapply star_step; [constructor|crush] | crush ] ].
+    + intros; simpl;
+      inversion S; clear S; subst.
+      inversion STAR; [| match goal with
+                         | [ H: _ R (PS (_ v) _) _ |- _ ] => inversion H
+                         end ].
+      eexists; split; [ apply star_refl | constructor; crush ].
+
+  - (* D *)
+    generalize dependent s2. generalize dependent S.
+    generalize dependent ls. generalize dependent ds.
+    unfold Compile; fold (@Compile R).
+    induction p.
+
+    + intros; simpl;
+      edestruct H; inversion S; clear S;
+      match goal with
+      | [ H: star (?STEP R) _ _ |- _ ] =>
+        inversion H; match goal with
+        | [ Hstep: STEP _ _ _ |- _ ] => inversion Hstep
+        end
+      end; subst;
+      [ constructor; eauto
+      | instantiate (2:=ls);
+        instantiate (1:=s2); eauto
+      | eauto
+      | exists x; split;
+        [ eapply star_step; [constructor|crush] | crush ] ].
+    + intros; simpl;
+      edestruct H; inversion S; clear S;
+      match goal with
+      | [ H: star (?STEP R) _ _ |- _ ] =>
+        inversion H; match goal with
+        | [ Hstep: STEP _ _ _ |- _ ] => inversion Hstep
+        end
+      end; subst;
+      [ constructor; eauto
+      | instantiate (2:=ls);
+        instantiate (1:=(setidx WholeDisk.eq_addr_dec s2 (d2w a) v)); resolve_disks
+      | resolve_disks
+      | exists x; split;
+        [ eapply star_step; [constructor|crush] | crush ] ].
+    + intros; simpl;
+      inversion S; clear S; subst.
+      inversion STAR; [| match goal with
+                         | [ H: _ R (PS (_ v) _) _ |- _ ] => inversion H
+                         end ].
+      eexists; split; [ apply star_refl | constructor; crush ].
+Qed.
+
+End LogPartsOnDisk.
